3a54bef90058343083dae860e4e53a8039ff6033
Lucas Severo <lucas.severo@captalys.com.br>
2020-07-16 22:27:08 -0300

    adicionago gui

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cnab-parser "0.2.1"
+(defproject cnab-parser "0.2.2"
   :description "Parser de CNAB de bancos brasileiros"
   :url "https://github.com/lsevero/cnab-parser"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,12 +6,23 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.6.0"]]
   :profiles {:dev {:repl-options {:init-ns cnab-parser.core}
-                   :dependencies [[org.slf4j/slf4j-log4j12 "1.7.1"]]
+                   :dependencies [[org.slf4j/slf4j-log4j12 "1.7.1"]
+                                  [seesaw "1.5.0"]
+                                  [cheshire "5.10.0"]]
                    :plugins [[cider/cider-nrepl "0.24.0"]]
                    :source-paths ["src" "test" "dev"]
                    :resource-paths ["resources" "resources-dev"]
                    :main cnab-parser.main
-                   }}
+                   }
+             :uberjar {:aot :all
+                       :repl-options {:init-ns cnab-parser.core}
+                       :dependencies [[org.slf4j/slf4j-log4j12 "1.7.1"]
+                                      [seesaw "1.5.0"]
+                                      [cheshire "5.10.0"]]
+                       :plugins [[cider/cider-nrepl "0.24.0"]]
+                       :source-paths ["src" "test" "dev"]
+                       :resource-paths ["resources" "resources-dev"]
+                       :main cnab-parser.main}}
   :source-paths ["src"]
   :resource-paths ["resources"]
   :repl-options {:port 17001}
